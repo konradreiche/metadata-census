2b52bf8b44b19d5226227320e8969083a43b8672
Konrad Reiche <konrad.reiche@gmail.com>
2013-04-15 00:22:14 +0200

    Create new Rails project
    Added a default gitignore file and generated a new Ruby on Rails
    project.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MetadataCensus::Application.config.secret_token = '1fdb79ab5f28b4845e6b50ed80533394cd8608f01b1f6454f22268ae1d731c8878b5451cda3143ce1b03e9858406e575e5651de94deac376bfb760d1646eacb7'
