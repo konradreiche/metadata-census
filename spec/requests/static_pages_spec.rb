717739e56a91eb2f55f0581b7120438c79c72cb4
Konrad Reiche <konrad.reiche@gmail.com>
2013-04-20 01:40:43 +0200

    Make test code pithier
    By using further Capybara primitives the test code could be refactored.
    A utilities files helps by determining the proper page title. All files
    inside spec/support are automatically included by RSpec.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,16 +2,12 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
+  subject { page }
+
   describe "Home Page" do
+    before { visit root_path }
 
-    it "should have the content 'Welcome!'" do
-      visit root_path
-      page.should have_selector('p', :text => 'Welcome!')
-    end
-
-    it "should not have a custom page title" do
-      visit root_path
-      page.should_not have_selector('title', :text => "| Home")
-    end
+    it { should have_selector 'p', text: 'Welcome!' }
+    it { should have_selector 'title', text: full_title('') }
   end
 end
