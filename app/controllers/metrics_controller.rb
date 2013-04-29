e9c33d8a886fb9b3b00da8b042233fa03eaba6a7
Konrad Reiche <konrad.reiche@gmail.com>
2013-04-29 16:17:24 +0200

    Refactor metric controller
    I have refactored the metric computation calls in the metric controller
    a bit by eliminating redundant code in the initialization of each call.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

--- app/controllers/metrics_controller.rb
+++ app/controllers/metrics_controller.rb
@@ -5,64 +5,51 @@ class MetricsController < ApplicationController
     @selected = @repositories.first if @selected.nil?
   end
 
-  def completeness(repository)
-
-    schema = JSON.parse File.read 'public/ckan-schema.json'
+  def apply_metric(repository, metric, name, *args)
     scores = []
 
     documents = Tire.search 'metadata' do
       query { string 'repository:' + repository.name }
     end.results
 
-
     for entry in documents
-      metric = Metrics::Completeness.new
       document = JSON.parse entry.to_json
-      metric.compute document, schema
+      metric.compute document, *args
       scores << metric.score
     end
-
+    
     scores.sort!
     minimum = scores.first
     maximum = scores.last
     average = scores.inject(:+) / scores.length
     median = scores[scores.length / 2]
     
-    repository.completeness = Score.new(minimum, maximum, average, median)
+    result = Score.new(minimum, maximum, average, median)
+    repository.send "#{name}=", result
     repository.update_index
         
     median
   end
 
-  def weighted_completeness(repository)
-    documents = Tire.search 'metadata' do
-      query { string 'repository:' + repository }
-    end.results
-
+  def completeness(repository)
     schema = JSON.parse File.read 'public/ckan-schema.json'
-    scores = []
+    metric = Metrics::Completeness.new
+    apply_metric(repository, metric, 'completeness', schema)
+  end
 
-    for entry in documents
-      metric = Metrics::WeightedCompleteness.new 'public/ckan-weight.yml'
-      document = JSON.parse entry.to_json
-      metric.compute document, schema
-      scores << metric.score
-    end
-    scores.inject(:+) / scores.length
+  def weighted_completeness(repository)
+    schema = JSON.parse File.read 'public/ckan-schema.json'
+    metric = Metrics::WeightedCompleteness.new 'public/ckan-weight.yml'
+    apply_metric(repository, metric, 'weighted_completeness', schema)
   end
 
   def richness_of_information(repository)
     documents = Tire.search 'metadata' do
-      query { string 'repository:' + repository }
-    end.results.map { |doc| JSON.parse doc.to_json }
+      query { string 'repository:' + repository.name }
+    end.results
 
-    scores = []
     metric = Metrics::RichnessOfInformation.new documents
-    for document in documents
-      metric.compute document
-      scores << metric. score
-    end
-    scores.inject(:+) / scores.length
+    apply_metric(repository, metric, 'richness_of_information')
   end
 
   def compute
