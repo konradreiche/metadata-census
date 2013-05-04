25c600026e4f784db2f0c82128f76604393be767
Konrad Reiche <konrad.reiche@gmail.com>
2013-05-04 18:08:42 +0200

    Fix failing tests
    The tests I have written so far failed due to different reasons. This
    commit fixes that.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

--- spec/models/metrics/completeness_spec.rb
+++ spec/models/metrics/completeness_spec.rb
@@ -1,22 +1,22 @@
 require 'spec_helper'
 
-describe Completeness do
+describe Metrics::Completeness do
   it "counts the number of total fields and fields with a non-null value" do
 
-    data = { 'author' => 'Donald E. Knuth',
-             'title' => 'The Art of Computer Programming, Volume 1',
-             'subtitle' => 'Fundamental Algorithms',
-             'language' => 'English' }
+    data = { :author => 'Donald E. Knuth',
+             :title => 'The Art of Computer Programming, Volume 1',
+             :subtitle => 'Fundamental Algorithms',
+             :language => 'English' }
 
-    schema = { 'type' => 'object',
-               'properties' => { 'author'    => { 'type' => 'string'  },
-                                 'title'     => { 'type' => 'string'  },
-                                 'subtitle'  => { 'type' => 'string'  },
-                                 'publisher' => { 'type' => 'string'  },
-                                 'language'  => { 'type' => 'string'  },
-                                 'pages'     => { 'type' => 'integer' } } }
+    schema = { :type       => 'object',
+               :properties => { :author    => { :type => 'string'  },
+                                :title     => { :type => 'string'  },
+                                :subtitle  => { :type => 'string'  },
+                                :publisher => { :type => 'string'  },
+                                :language  => { :type => 'string'  },
+                                :pages     => { :type => 'integer' }}}
 
-    metric = Completeness.new
+    metric = Metrics::Completeness.new
     metric.compute data, schema
     expect(metric.fields).to be 6
     expect(metric.fields_completed).to be 4
--- spec/views/metrics/overview.html.erb_spec.rb
+++ spec/views/metrics/overview.html.erb_spec.rb
@@ -3,7 +3,11 @@ require 'spec_helper'
 describe "metrics/overview.html.erb" do
 
   it "should display the page" do
-
+    assign(:repositories, [
+      stub_model(Repository, :name => 'data.gov.com', :type => 'CKAN',
+                 :url => 'http://data.gov.com', :datasets => 100000,
+                 :latitude => 0.0, :longitude => 0.0)
+    ])
     render
     expect(rendered).to include 'Metrics'
   end
