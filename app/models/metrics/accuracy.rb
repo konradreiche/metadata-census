fbaf804947d7ef4d57c1d7573b89663ec6c1dd71
Konrad Reiche <konrad.reiche@gmail.com>
2013-05-04 18:48:39 +0200

    Add accuracy metric skeleton
    Just added the files for the accuracy metric.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

---
+++ app/models/metrics/accuracy.rb
@@ -0,0 +1,5 @@
+module Metrics
+
+  class Accuracy
+  end
+end
---
+++ spec/models/metrics/accuracy_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+
+describe Metrics::Accuracy do
+  it "counts the number of correct specified MIME types for resources" do
+  end
+end
