69fbd21d8a8b4e307b779fc650789baaab016a71
Konrad Reiche <konrad.reiche@gmail.com>
2013-05-17 12:02:37 +0200

    Add more tests for the word splitter
    Obiously one test is ridicilous too few, so I just added two more.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

--- spec/models/metrics/accessibility_spec.rb
+++ spec/models/metrics/accessibility_spec.rb
@@ -3,12 +3,20 @@ require 'spec_helper'
 describe Metrics::Accessibility do
 
   it "should split a given text into its words" do
+    
     text = "Estimates of average farm rent prices by farm type."
     expectations = ['Estimates', 'of', 'average', 'farm', 'rent', 'prices',
                     'by', 'farm', 'type']
 
     words = Metrics::Accessibility.split_to_words(text)
     words.should match_array expectations
+
+    words = Metrics::Accessibility.split_to_words("")
+    words.should match_array []
+
+    words = Metrics::Accessibility.split_to_words("Estimates")
+    words.should match_array ['Estimates']
+
   end
   
 end
