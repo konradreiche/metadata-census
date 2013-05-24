b1af2e7ffb2c471bd25c9f220d0df132611d0c57
Konrad Reiche <konrad.reiche@gmail.com>
2013-05-24 14:00:06 +0200

    Add missing argument for completeness worker
    There was a missing argument, as well as as a missing call to self.class
    for symbolize_keys which prevented the completeness metric from working.
    This commit fixes that.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

--- app/workers/completeness_metric_worker.rb
+++ app/workers/completeness_metric_worker.rb
@@ -3,9 +3,9 @@ class CompletenessMetricWorker < MetricsWorker
   def perform(repository_name)
     repository = Repository.find repository_name
     schema = JSON.parse File.read 'public/ckan-schema.json'
-    schema = symbolize_keys schema
+    schema = self.class.symbolize_keys schema
     metric = Metrics::Completeness.new
-    compute(repository, metric)
+    compute(repository, metric, schema)
   end
 
 end
