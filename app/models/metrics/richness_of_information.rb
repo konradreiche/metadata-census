527d9b7fd532a1360c1976c117fbd9a841f13f0b
Konrad Reiche <konrad.reiche@gmail.com>
2013-05-28 16:36:50 +0200

    Refactor the term frequency methods
    I have applied some refactoring to the term frequency method of the
    richness of information metric in order to make it easier to test.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

--- app/models/metrics/richness_of_information.rb
+++ app/models/metrics/richness_of_information.rb
@@ -2,10 +2,10 @@ module Metrics
 
   class RichnessOfInformation < Metric
 
-    attr_reader :score
+    attr_reader :score, :document_numbers, :document_frequency
 
     def initialize(datasets)
-      @document_frequency = Hash.new []
+      @document_frequency = Hash.new(0)
       @document_numbers = 0.0
       for entry in datasets
         index_field(entry, :notes, entry[:id])
@@ -26,18 +26,30 @@ module Metrics
       end
     end
 
-    def compute(data)
-      term_frequency = Hash.new 0
+    def self.term_frequency(data)
+      term_frequency = Hash.new(0)
       doc_length = 0
-      for resource in data[:resources]
-        if resource.has_key?(:description) and not resource[:description].nil?
-          resource[:description].downcase.split(/\W+/).each do |w|
-            term_frequency[w] += 1
-            doc_length += 1
-          end
-        end
-      end
 
+      term_frequency, doc_length = count_words(data, :notes, term_frequency, doc_length)
+      for resource in data[:resources]
+        result = count_words(resource, :description, term_frequency, doc_length)
+        term_frequency, doc_length = result
+      end
+      [term_frequency, doc_length.to_f]
+    end
+
+    def self.count_words(entity, field, term_frequency, doc_length)
+      unless entity[field].nil?
+        entity[field].downcase.split(/\W+/).each do |word|
+          term_frequency[word] += 1
+        end
+        doc_length += 1
+      end
+      [term_frequency, doc_length]
+    end
+
+    def compute(data)
+      term_frequency, doc_length = self.class.term_frequency(data[:resources])
       factors = []
       term_frequency.each do |word, count| 
         factors << count / doc_length.to_f * Math.log(@document_numbers / @document_frequency[word].length)
