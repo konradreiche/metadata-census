d149eacc606b66c2866b9fd2713b51e475df3bd8
Konrad Reiche <konrad.reiche@gmail.com>
2013-06-10 15:11:40 +0200

    Group accessibility results per source portal
    For GovData.de the accessibility results can be grouped by the
    meta-metadata field original_metadata_portal. I am retrieving all
    records which have this field set and then average the metric score
    mapped to the portal.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

--- app/controllers/metrics/accessibility_controller.rb
+++ app/controllers/metrics/accessibility_controller.rb
@@ -7,6 +7,19 @@ class Metrics::AccessibilityController < ApplicationController
     else
       @repository = @repositories.first
     end
+
+    if @repository.id == 'GovData.de'
+      gon.portals = group_by_portal
+    end
+  end
+
+  def group_by_portal
+    field = 'extras.metadata_original_portal'
+    grouped = Hash.new { |h, k| h[k] = [] }
+    for record in @repository.metadata_with_field(field)
+      grouped[record[:extras][:metadata_original_portal]] << record[:accessibility]
+    end
+    grouped.each { |portal, scores| grouped[portal] = scores.inject(:+)/scores.length }
   end
 
 end
