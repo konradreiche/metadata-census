faa23758b24c060d5b82a7a1026ea71b43a3d049
Konrad Reiche <konrad.reiche@gmail.com>
2013-07-08 23:57:15 +0200

    Extend metrics overview to fix the build again
    After the last commit one test was failing. This commit fixes that again
    by providing sufficient stub models. Also removed the helper spec.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

--- spec/views/metrics/overview.html.erb_spec.rb
+++ spec/views/metrics/overview.html.erb_spec.rb
@@ -3,11 +3,12 @@ require 'spec_helper'
 describe "metrics/overview.html.erb" do
 
   it "should display the page" do
-    assign(:repositories, [
-      stub_model(Repository, :name => 'data.gov.com', :type => 'CKAN',
-                 :url => 'http://data.gov.com', :datasets => 100000,
-                 :latitude => 0.0, :longitude => 0.0)
-    ])
+    repository = stub_model(Repository, :name => 'data.gov.com', :type => 'CKAN',
+                            :url => 'http://data.gov.com', :datasets => 100000,
+                            :latitude => 0.0, :longitude => 0.0)
+
+    assign(:repositories, [repository])
+    assign(:selected, repository)
     render
     expect(rendered).to include 'Metrics'
   end
