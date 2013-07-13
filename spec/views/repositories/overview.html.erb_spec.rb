8403fd1afc908eac73da7ecd6fca9de13bf3f12d
Konrad Reiche <konrad.reiche@gmail.com>
2013-07-13 16:38:02 +0200

    Fix test by assigning total datasets value to stub
    Since I had not defined a total datasets value for the model stub in the
    test, the query of the total method was executed. Without indexes on the
    ElasticSearch instance this fails. Hence, I just added this value.
    
    With this change I also added assertions to check the formatting of the
    rendered value.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

--- spec/views/repositories/overview.html.erb_spec.rb
+++ spec/views/repositories/overview.html.erb_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 
 describe "repositories/overview.html.erb" do
+  
   it "should display all the repositories" do
-
     assign(:repositories, [
       stub_model(Repository, :name => 'Open Data Portal', :type => 'Database',
-                 :url => 'http://example.com/portal'),
+                 :url => 'http://example.com/portal', :total => 3000),
       stub_model(Repository, :name => 'Open Government', :type => 'Platform',
-                 :url => 'http://example.com/platform')
+                 :url => 'http://example.com/platform', :total => 7000)
     ])
 
     render
@@ -19,5 +19,9 @@ describe "repositories/overview.html.erb" do
     expect(rendered).to include 'Open Government'
     expect(rendered).to include 'Platform'
     expect(rendered).to include 'example.com/platform'
+
+    expect(rendered).to include '3,000'
+    expect(rendered).to include '7,000'
   end
+
 end
