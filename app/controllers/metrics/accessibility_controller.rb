cc33507fc6faea071cc2b6475a7493cac50a09c4
Konrad Reiche <konrad.reiche@gmail.com>
2013-07-18 14:45:39 +0200

    Fix Accessibility metric diagram
    The problem with the diagram was that the data input exceeded the d3.js
    configuration. My solution thins out the values. I am not completely
    happy with the solution, since it can only be an intermediate solution.
    With the current implementation it is still possible that there are 100
    values which might be too much.
    
    With all probability I have to think about an alternative way to make
    the metric results accessible. In addition this commit also implements
    the normalization of the Accessibility results. This commit fixes #1.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

--- app/controllers/metrics/accessibility_controller.rb
+++ app/controllers/metrics/accessibility_controller.rb
@@ -10,25 +10,52 @@ class Metrics::AccessibilityController < ApplicationController
 
     if @repository.id == 'GovData.de'
       field = 'extras.metadata_original_portal'
-      gon.grouped_accessibility = group_by(field)
+      gon.grouped_accessibility = normalize(@repository, field)
     else
       field = 'groups'
-      gon.grouped_accessibility = group_by(field)
+      gon.grouped_accessibility = normalize(@repository, field)
     end
   end
 
-  def group_by(field)
+  def normalize(repository, field)
+    metadata = repository.metadata_with_field(field)
+    values = metadata.map { |record| record[:accessibility] }
+    max = values.max
+    min = values.min
+    range = max - min
+
     accessors = field.split(/\./)
     grouped = Hash.new { |h, k| h[k] = [] }
-    for record in @repository.metadata_with_field(field)
+    metadata.each do |record|
       group = accessors.inject(record) { |group, a| group[a.to_sym] }
+      # if there is more than one group
+      value = 100 * (record[:accessibility] - min) / range
       if group.is_a? Array
-        group.each { |g| grouped[g] << record[:accessibility]}
+        group.each { |g| grouped[g] << value }
       else
-        grouped[group] << record[:accessibility]
+        grouped[group] << value
       end
     end
-    grouped.each { |portal, scores| grouped[portal] = scores.inject(:+)/scores.length }
+
+    grouped.each do |portal, scores|
+      grouped[portal] = scores.inject(:+) / scores.length
+    end
+
+    thin_out(grouped)
   end
 
+  def thin_out(grouped)
+    values = []
+    grouped.delete_if do |key|
+      range = grouped[key].to_i
+      if values.include?(range)
+        true
+      else
+        values << range
+        false
+      end
+    end
+    grouped
+  end
+  
 end
