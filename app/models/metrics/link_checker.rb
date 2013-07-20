d2102db91e5c978236cfa8c284a1e3ef4b3b3487
Konrad Reiche <konrad.reiche@gmail.com>
2013-07-20 18:10:52 +0200

    Add Typhoeus request code for the Link Checker
    Just added the code to determine whether an URL is working or not. This
    can be done by relying on the response.success? method, as in every
    other case either another response code has been delivered or the
    request timed out.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

--- app/models/metrics/link_checker.rb
+++ app/models/metrics/link_checker.rb
@@ -7,8 +7,11 @@ module Metrics
 
     def initialize(metadata, worker=nil)
       @worker = worker
+      @processed = 0
+      @requests = 0
       @total = metadata.length
-      @resource_availability = Hash.new
+
+      @resource_availability = Hash.new { |h, k| h[k] = Hash.new }
       metadata.each_with_index do |dataset, i|
         dataset[:resources].each do |resource|
           @resources += 1
@@ -17,6 +20,26 @@ module Metrics
         end
         @worker.at(i + i, @total)
       end
+    end
+
+    def enqueue_request(id, url)
+
+      config = {:method => :head,
+                :timeout => 20,
+                :connecttimeout => 10,
+                :nosignal => true}
+
+      @resource_availability[id][url] = false
+      request = Typhoeus::Request.new(url, config)
+      request.on_complete do |response|
+        if response.success?
+          @resource_availability[id] = true
+        end
+        @worker.at(@processed + 1, @requests)
+        @processed += 1
+      end
+      @dispatcher.queue(request)
+      @requests += 1
     end
 
   end
