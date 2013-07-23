1ce9595403fe7c6f00f379e90ff55d1eece70197
Konrad Reiche <konrad.reiche@gmail.com>
2013-07-24 00:33:51 +0200

    Display URLs and their request responses
    Added the code to list the URLs and their request responses in a table.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

--- app/controllers/metrics/link_checker_controller.rb
+++ app/controllers/metrics/link_checker_controller.rb
@@ -2,6 +2,12 @@ class Metrics::LinkCheckerController < MetricsReportController
 
   def report
     super
+    @url_request_responses = Hash.new
+    metadata = @repository.fetch_metadata
+    metadata.each do |document|
+      @url_request_responses.merge! document[:link_checker_details]
+    end
+    logger.info @url_request_responses.first
   end
 
 end
