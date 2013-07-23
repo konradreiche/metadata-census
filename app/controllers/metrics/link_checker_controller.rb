8081a3d4c03ef19cd6f178d960914f78f37c3109
Konrad Reiche <konrad.reiche@gmail.com>
2013-07-24 00:43:41 +0200

    Style Link Checker report page
    Just added similar styling to the Link Checker report page as on the
    Completeness report page.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

--- app/controllers/metrics/link_checker_controller.rb
+++ app/controllers/metrics/link_checker_controller.rb
@@ -1,5 +1,7 @@
 class Metrics::LinkCheckerController < MetricsReportController
 
+  helper_method :success?
+
   def report
     super
     @url_request_responses = Hash.new
@@ -7,7 +9,14 @@ class Metrics::LinkCheckerController < MetricsReportController
     metadata.each do |document|
       @url_request_responses.merge! document[:link_checker_details]
     end
-    logger.info @url_request_responses.first
+  end
+
+  def success?(code)
+    if code.is_a?(Fixnum) and code >= 200 and code < 300
+      'successful'      
+    else
+      'unsuccessful'
+    end
   end
 
 end
