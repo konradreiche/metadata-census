5be7926f644c28b326777b93f4d691d8d0417e7d
Konrad Reiche <konrad.reiche@gmail.com>
2013-07-25 22:15:08 +0200

    Change parameter for the repository report
    Since the page name is repository I have changed the parameter from
    repository to show.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -2,7 +2,7 @@ class ReportController < ApplicationController
 
   def repository
     @repositories = Repository.all
-    @repository = params[:repository] || @repositories.first.name
+    @repository = params[:show] || @repositories.first.name
     @repository = Repository.find(@repository)
   end
 
