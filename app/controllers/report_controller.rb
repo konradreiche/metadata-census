136ee36f843b0d356864b39691d3ffc847719902
Konrad Reiche <konrad.reiche@gmail.com>
2013-07-25 21:47:18 +0200

    Add layout file for repository report
    For the repository report card I have added a layout page.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -1,4 +1,9 @@
 class ReportController < ApplicationController
+
   def repository
+    @repositories = Repository.all
+    @repository = params[:repository] || @repositories.first.name
+    @repository = Repository.find(@repository)
   end
+
 end
