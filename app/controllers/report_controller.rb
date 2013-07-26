9e3b9e8bb08591b7d3d9f3be48b8acd3620daeb4
Konrad Reiche <konrad.reiche@gmail.com>
2013-07-26 19:00:23 +0200

    Compute average score of a repository
    The average score combines all the metric scores so far by averaging
    them. For this I have added a list of metrics which need normalization.
    When a metric is picked that is part of the list the normalize algorithm
    as defined in the Metrics module is applied.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -4,6 +4,25 @@ class ReportController < ApplicationController
     @repositories = Repository.all
     @repository = params[:show] || @repositories.first.name
     @repository = Repository.find(@repository)
+    @score = average_score(@repository)
+  end
+
+  def average_score(repository)
+    metrics = Metrics::IDENTIFIER
+    sum = metrics.inject(0.0) do |sum, metric|
+      score = repository.send(metric)
+      unless score.nil?
+        value = score[:average]
+        if Metrics::NORMALIZE.include?(metric)
+          value = Metrics::normalize(metric, [value]).first
+        end
+      else
+        value = 0.0
+      end
+
+      sum + value
+    end
+    sum / metrics.length
   end
 
 end
--- app/models/metrics.rb
+++ app/models/metrics.rb
@@ -7,7 +7,23 @@ module Metrics
                 :accessibility,
                 :link_checker]
 
-  def normalize(metric, values)
+  NORMALIZE = [:richness_of_information,
+               :accessibility]
+
+  def self.normalize(metric, values)
+    scores = []
+    repositories = Repository.all
+    repositories.each do |repository|
+      score = repository.send(metric)
+      unless score.nil?
+        scores << score[:minimum]
+        scores << score[:maximum]
+      end
+    end
+    min = scores.min
+    max = scores.max
+    range = max - min
+    values.map { |value| 100 * (value - min) / range }
   end
 
 end
