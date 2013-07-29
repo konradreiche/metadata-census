9d2cc0000ee39021610098f994f32d378a14cf65
Konrad Reiche <konrad.reiche@gmail.com>
2013-07-29 17:08:10 +0200

    Style repository selector for admin control panel
    I have styled the repository selector for the admin control panel. For
    this I needed to use the btn-toolbar class, otherwhise it was not
    possible to center the dropdown menu with letting the menu items
    disappear.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,7 +1,10 @@
 class AdminController < ApplicationController
+  include Concerns::Repository
 
   def control
+    load_repositories(:repository)
     @metrics = Metrics::IDENTIFIER
   end
+
 
 end
--- app/controllers/concerns/repository.rb
+++ app/controllers/concerns/repository.rb
@@ -1,9 +1,10 @@
 module Concerns::Repository
 
-    def load(parameter)
-      @repositories = Repository.all
-      @repository = params[parameter] || @repositories.first.name
-      @repository = Repository.find(@repository)
-    end
+  def load_repositories(parameter)
+    @repositories = ::Repository.all
+    @repository = params[parameter] || @repositories.first.name
+    @repository = ::Repository.find(@repository)
+    gon.repository = @repository
+  end
 
 end
