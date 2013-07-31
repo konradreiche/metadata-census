e960a118c9fa0f29bbd5fc4a75a01414d7d4d9b8
Konrad Reiche <konrad.reiche@gmail.com>
2013-07-31 15:09:50 +0200

    Add status helper method
    This status helper method should help to display whether metric results
    for a certain repository are available or not.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,11 +1,16 @@
 class AdminController < ApplicationController
   include Concerns::Repository
 
+  helper_method :status
+
   def control
     load_repositories(:repository)
     @metrics = Metrics::IDENTIFIER
     gon.metrics = @metrics
   end
 
+  def status(metric)
+    not @repository.send(metric).nil?
+  end
 
 end
