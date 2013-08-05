6154f45e31811f9266981ec632d814fafc5cec6f
Konrad Reiche <konrad.reiche@gmail.com>
2013-08-05 12:15:22 +0200

    Refactor helper methods for metric report anchors
    Just refactored the helper methods to be based on the same method used
    to construct the URL.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

--- app/helpers/report_helper.rb
+++ app/helpers/report_helper.rb
@@ -1,16 +1,17 @@
 module ReportHelper
 
-  def create_metric_report_link(metric)
-    metric = Metrics::get_url_representation(metric)
+  def create_metric_report_button(metric)
     icon = tag(:strong, class: 'icon-th')
-    url = "/report/metric?show=#{metric}&repository=#{@repository.name}"
-    content_tag(:a, icon, class: 'btn', href: url)
+    content_tag(:a, icon, class: 'btn', href: metric_url(metric))
   end
 
-  def create_metric_link(metric)
-    metric= Metrics::get_url_representation(metric)
-    url = "/report/metric?show=#{metric}&repository=#{@repository.name}"
-    content_tag(:a, metric.to_s.titlecase, href: url)
+  def create_metric_report_link(metric)
+    content_tag(:a, metric.to_s.titlecase, href: metric_url(metric))
+  end
+
+  def metric_url(metric)
+    metric = Metrics::get_url_representation(metric)
+    "/report/metric?show=#{metric}&repository=#{@repository.name}"
   end
 
   def create_repository_link(repository)
