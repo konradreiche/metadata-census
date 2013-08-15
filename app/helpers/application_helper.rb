1006edea311e6883fd4d882498328fe66070120b
Konrad Reiche <konrad.reiche@gmail.com>
2013-08-15 17:56:17 +0200

    Activate the navigation tabs dynamically
    I have added a helper to dynamically set the active class for the
    navigation tabs in the header.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  def create_navbar_button(display, path)
+    cls = current_page?(path) ? 'active' : nil
+    content_tag(:li, link_to(display, path), class: cls)
+  end
+
 end
