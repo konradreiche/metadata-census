976fb7c95f174574b3fe59e452e64fb0523ddd7e
Konrad Reiche <konrad.reiche@gmail.com>
2013-08-18 22:58:09 +0200

    Fix success determination for response code
    I used the (and) operator which has a lower precedence than the
    assignment operator (=), hence I have replaced it with the (&&)
    operator.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

--- app/helpers/metrics_helper/link_checker_helper.rb
+++ app/helpers/metrics_helper/link_checker_helper.rb
@@ -1,7 +1,7 @@
 module MetricsHelper::LinkCheckerHelper
 
   def response_td(response)
-    success = response.is_a?(Fixnum) and response >= 200 and response < 300
+    success = response.is_a?(Fixnum) && response >= 200 && response < 300
     cls = success ? 'successful' : 'unsuccessful'
     content_tag(:td, response, class: cls)
   end
