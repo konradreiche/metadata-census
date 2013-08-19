e83a6d32a63c30517152cb8f0fc4b66fa38c74dc
Konrad Reiche <konrad.reiche@gmail.com>
2013-08-19 22:32:21 +0200

    Fix default view on the link checker analysis
    It takes way too long to load all the link checker results. That is why
    I will start with the broken links and make the others available through
    the tool bar.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

--- app/assets/javascripts/application.js.coffee
+++ app/assets/javascripts/application.js.coffee
@@ -10,6 +10,7 @@
 # WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 # GO AFTER THE REQUIRES BELOW.
 #
+#= require angular.min.js
 #= require jquery
 #= require jquery_ujs
 #= require bootstrap
--- app/helpers/metrics_helper/link_checker_helper.rb
+++ app/helpers/metrics_helper/link_checker_helper.rb
@@ -1,8 +1,11 @@
 module MetricsHelper::LinkCheckerHelper
 
+  def successful?(code)
+    code.is_a?(Fixnum) && code >= 200 && code < 300
+  end
+
   def response_td(response)
-    success = response.is_a?(Fixnum) && response >= 200 && response < 300
-    cls = success ? 'successful' : 'unsuccessful'
+    cls = successful?(response) ? 'successful' : 'unsuccessful'
     content_tag(:td, response, class: cls)
   end
 
