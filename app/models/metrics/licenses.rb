2fcda4580bef8970fc9277bd0a3bb1aa98ca25df
Konrad Reiche <konrad.reiche@gmail.com>
2013-08-21 23:54:18 +0200

    Add more code to the license metric
    Just started to write some code for the license metric. I will have to
    find a good way to consolidate different data licenses and on what terms
    it is open/valid and on what terms it is not.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

--- app/models/metrics/licenses.rb
+++ app/models/metrics/licenses.rb
@@ -4,11 +4,18 @@ module Metrics
 
     attr_reader :score
 
+    def license_valid?(id)
+      licenses = { "dl-de-by-1.0" => true }
+      licenses[id]
+    end
+
     def initialize
       @score = 0.0
     end
 
     def compute(record)
+      license = record[:license_id]
+      @score = 1.0 if license_valid?(license)
     end
 
   end
