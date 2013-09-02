117df6395a42afd24c00d927b36e871b34a3d615
Konrad Reiche <konrad.reiche@gmail.com>
2013-09-02 17:47:45 +0200

    Implement record selection logic
    Just finished the record selection logic. With some short method changes
    and performing a replace in the array of document ids I was able to
    implement the link creation in a concise way.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

--- app/helpers/metrics_helper.rb
+++ app/helpers/metrics_helper.rb
@@ -1,11 +1,30 @@
 module MetricsHelper
 
-  def record_selector_entry(document)
-    score = "%6.2f%" % (document[@metric][:score] * 100)
+  ##
+  # Constructs an entry for the record selector based on a given document.
+  #
+  # The +current+ document is used to find the document to replace for the new
+  # link. The +new+ document is used for the menu item text and the link.
+  #
+  def record_selector_entry(current, new)
+    score = "%6.2f%" % (new[@metric][:score] * 100)
     score = score.gsub(' ', '&nbsp;')
-    text = "#{score} &#8212; #{document[:record][:name]}".html_safe
-    anchor = content_tag(:a, text, role: 'menuitem', tabindex: '-1', href: '#')
+    text = "#{score} &#8212; #{new[:record][:name]}".html_safe
+    href = record_selector_entry_link(current, new)
+    anchor = content_tag(:a, text, role: 'menuitem', tabindex: '-1', href: href)
     content_tag(:li, anchor, role: 'presentation')
+  end
+
+  ##
+  # Constructs the link for an entry of the record selector
+  #
+  def record_selector_entry_link(current, new)
+    documents = @documents.map { |document| document[:id] }
+    documents[documents.index(current[:id])] = new[:id]
+    query = documents.map do |document|
+      URI.unescape(document.to_query(:"documents[]"))
+    end * '&'
+    "?#{query}"
   end
 
 end
