877b6e9a16681079cb9a12841e017a40c40e5af9
Konrad Reiche <konrad.reiche@gmail.com>
2013-09-12 15:03:30 +0200

    Implement JSON archive filename parsing
    For the JSON archive importer I have written the code to parse the
    filenames and split them up. Apparently, I will be required to take
    the harvesting date from the filename itself. Also I have added the
    archives directory to gitignore.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -8,6 +8,13 @@ class AdminController < ApplicationController
   end
 
   def import
+    archives = Dir.glob('archives/**/*').select { |fn| File.file?(fn) }
+    @archives = archives.inject({}) do |result, archive|
+      parts = File.basename(archive).split('-')
+      date = DateTime.new(*parts[0..2].map(&:to_i))
+      result[archive.split('/').last] = date
+      result
+    end
   end
 
 end
