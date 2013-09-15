1256f700abebcfe33e104c56503830747d30f14a
Konrad Reiche <konrad.reiche@gmail.com>
2013-09-15 13:54:45 +0200

    Add list to display repository files
    The repository files will now be stored in the directory
    data/repositories. For multiple files I have also added
    a field to add a name, a label, to the file.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

--- app/controllers/admin/repositories_controller.rb
+++ app/controllers/admin/repositories_controller.rb
@@ -1,6 +1,17 @@
 class Admin::RepositoriesController < ApplicationController
+  helper_method :repository_count
 
   def new
+    @repository_files = repository_files
   end
 
+  def repository_files
+    files = Dir.glob('data/repositories/*.yml').select { |f| File.file?(f) }
+    files.map { |yaml| YAML.load_file(yaml).with_indifferent_access }
+  end
+
+  def repository_count(file)
+    file.except(:name).values.map(&:length).reduce(:+)
+  end
+    
 end
