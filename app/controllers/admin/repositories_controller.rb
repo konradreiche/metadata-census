77a8314596b83250ec6f1fcccf8cea617400e0ec
Konrad Reiche <konrad.reiche@gmail.com>
2013-09-17 01:11:24 +0200

    Implement parsing and displaying of JSON dumps
    The archived JSON data is now displayed in the administration view for
    importing repositories. They are listed, their meta-metadata is parsed
    and displayed. The import link is not working yet.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

--- app/controllers/admin/repositories_controller.rb
+++ app/controllers/admin/repositories_controller.rb
@@ -1,3 +1,6 @@
+require 'json/stream'
+require 'yajl'
+
 class Admin::RepositoriesController < ApplicationController
   helper_method :repository_count
 
@@ -19,7 +22,8 @@ class Admin::RepositoriesController < ApplicationController
   end
 
   def new
-    @repository_files = repository_files
+    @repository_files = repository_files()
+    @metadata_archives = metadata_archives()
   end
 
   def repository_files
@@ -27,6 +31,48 @@ class Admin::RepositoriesController < ApplicationController
     files.map do |yaml| 
       { yaml => YAML.load_file(yaml).with_indifferent_access }
     end.reduce(:merge)
+  end
+
+  ##
+  # Returns a list of available metadata archives.
+  #
+  def metadata_archives
+    options = { symbolize_keys: true }
+    archives = Dir.glob('data/archives/**/*').select { |f| File.file?(f) }
+
+    archives.inject({}) do |result, archive|
+      result[archive] = parse_meta_metadata(archive)
+      result
+    end
+  end
+
+  ##
+  # Parses the meta-metadata from the archive in a stream-based fashion.
+  #
+  def parse_meta_metadata(file)
+    result = {}
+    keys = [:id, :date]
+
+    pick_next = false
+    pick_key = nil
+
+    parser = JSON::Stream::Parser.new do
+      value { |value| result[pick_key] = value if pick_next }
+
+      key do |key|
+        pick_next = keys.include?(key.to_sym) 
+        pick_key = key.to_sym
+      end
+    end
+
+    File.open(file, 'r') do |file|
+      file.each_char do |char|
+        parser << char
+        break if result.keys.sort == keys.sort
+      end
+    end
+
+    result
   end
 
   def repository_count(yaml)
--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -7,14 +7,4 @@ class AdminController < ApplicationController
     load_metrics()
   end
 
-  def importer
-    archives = Dir.glob('archives/**/*').select { |fn| File.file?(fn) }
-    @archives = archives.inject({}) do |result, archive|
-      parts = File.basename(archive).split('-')
-      date = DateTime.new(*parts[0..2].map(&:to_i))
-      result[archive.split('/').last] = date
-      result
-    end
-  end
-
 end
