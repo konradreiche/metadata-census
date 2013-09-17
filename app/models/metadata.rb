ae8e639c40af49d080a5769c6dbeb5cd25f21cae
Konrad Reiche <konrad.reiche@gmail.com>
2013-09-18 00:25:59 +0200

    Customize metadata document identifier
    With this customization I can import the same metadata records as often
    as I want. There won't be any duplicates.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

--- app/models/metadata.rb
+++ app/models/metadata.rb
@@ -1,3 +1,5 @@
+require 'digest'
+
 class Metadata
   include Mongoid::Document
 
@@ -7,6 +9,15 @@ class Metadata
   field :repository
   field :date
   field :type
+
+  ##
+  # Custom default identifier which comprisees of the harvesting date, the
+  # repository identifier and the metadata record identifier.
+  #
+  field :_id, default: -> do
+    unique = [date, repository.id, record[:id]].join("-")
+    Digest::MD5.hexdigest(unique)
+  end
 
   belongs_to :repository
 
