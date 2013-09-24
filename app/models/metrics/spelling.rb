1bb5ce6115bc98809f542ea1d5713ab6974ac0fa
Konrad Reiche <konrad.reiche@gmail.com>
2013-09-25 01:34:04 +0200

    Skip empty text in the spelling metric
    Empty text resulted in NaN scores for the per metadata record analysis.
    Hence, I have added a skip statement to avoid recording these fields.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

--- app/models/metrics/spelling.rb
+++ app/models/metrics/spelling.rb
@@ -19,15 +19,19 @@ module Metrics
     end
 
     def compute(record)
-      analysis = Hash.new
+      analysis = []
       all_words = all_mistakes = 0
       speller = aspell(detect_language(record))
 
       @fields[:text].each do |accessor|
         value = self.class.value(record, accessor)
+
         Array(value).each_with_index do |text, i|
+          next if skip?(text)
+
           misspelled = []
           words = mistakes = 0
+
           words(text.to_s).each do |word|
             all_words = words += 1
             next if word.length < 7
@@ -37,13 +41,17 @@ module Metrics
               misspelled << word
             end
           end
+
           path  = value.is_a?(Array) ? accessor + [i + 1] : accessor
           score = 1.0 - mistakes.fdiv(words)
-          analysis[path] = { score: score, misspelled: misspelled.uniq }
+
+          analysis << { field: path, score: score,
+                        misspelled: misspelled.uniq }
         end
       end
 
       score = 1.0 - all_mistakes.fdiv(all_words)
+      
       return score, analysis
     end
 
