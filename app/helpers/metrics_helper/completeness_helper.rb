7795e3f998157a427ae30ef4dff45de23012c5c3
Konrad Reiche <konrad.reiche@gmail.com>
2013-09-25 20:07:50 +0200

    Fix id class mix up and add title to table cells
    I have mixed up identifier and class for the analysis and statistics
    tab. Cell content that is too long, overflows, is cut down. In order
    to get the full content I have added a title attribute.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

--- app/helpers/metrics_helper/completeness_helper.rb
+++ app/helpers/metrics_helper/completeness_helper.rb
@@ -3,8 +3,9 @@ module MetricsHelper::CompletenessHelper
   def value(value)
     blank = Metrics.blank?(value)
     value = nil if Metrics.blank?(value)
+    cls = blank ? 'incomplete' : 'complete'
 
-    content_tag(:td, value, class: blank ? 'incomplete' : 'complete')
+    content_tag(:td, value, class: cls , title: value)
   end
 
 end
