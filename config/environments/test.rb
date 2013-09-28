f2632995c8ebe1e49b07e23a47ab13671b5d6735
Konrad Reiche <konrad.reiche@gmail.com>
2013-09-28 18:57:47 +0200

    Adjust unit tests for intrinsic metric
    For the new intrinsic metric implementation I have adjusted the unit
    tests accordingly.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,4 +1,7 @@
 MetadataCensus::Application.configure do
+  
+  Moped.logger.level = Logger::INFO
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # The test environment is used exclusively to run your application's
--- spec/models/metrics/intrinsic_precision_spec.rb
+++ spec/models/metrics/intrinsic_precision_spec.rb
@@ -10,13 +10,7 @@ describe Metrics::IntrinsicPrecision do
     expect(metric.corpus(record)).to eq("This is an English text. Dataset")
   end
 
-  it "#detect_language" do
-    record = { notes: "This is an English text.",
-               resources: { description: "Dataset" } }
-
-    metric = Metrics::IntrinsicPrecision.new
-    expect(metric.language(record)).to eq(:english)
-
+  it "#language" do
     record = { notes: "Das ist ein deutscher Text.",
                resources: { description: "Datensatz" } }
 
@@ -24,22 +18,27 @@ describe Metrics::IntrinsicPrecision do
     expect(metric.language(record)).to eq(:german)
   end
 
-  it "should determine the number of spelling mistakes" do
-    record = { notes: "Is this a aaaatext?" }
+  it "detect common spelling mistakes" do
+    record = { notes: "Die Addresse des Hauses lautet" }
+
     metric = Metrics::IntrinsicPrecision.new
-   # score, analysis = metric.compute(record)
+    score, analysis = metric.compute(record)
+
     accessor = [:notes]
-   # expect(score).to be(0.75) 
-   # expect(analysis.first).to eq({ field: accessor, score: 0.75, misspelled: ['aaaatext'] })
+    expect(score).to be(0.0) 
+    expect(analysis.first).to eq({ field: accessor, score: 0.0, misspelled: ['Addresse'] })
   end
 
   it "should also work on nested records" do
-    record = { resources: { description: 'This is a text.' } }
+    record = { notes: 'Die Adresse des Hauses lautet',
+               resources: { description: 'Turbolenzen' } }
+
     metric = Metrics::IntrinsicPrecision.new
-   # score, analysis = metric.compute(record)
-    accessor = [:resources, :description]
-   # expect(score).to be(1.0) 
-   # expect(analysis.first).to eq({ field: accessor, score: 1.0, misspelled: []})
+    score, analysis = metric.compute(record)
+
+    expect(score).to be(0.0) 
+    expect(analysis[0]).to eq({ field: [:notes], score: 1.0, misspelled: [] })
+    expect(analysis[1]).to eq({ field: [:resources, :description], score: 0.0, misspelled: ['Turbolenzen'] })
   end
 
 end
