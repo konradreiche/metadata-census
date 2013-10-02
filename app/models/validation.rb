0a1cc1d66a934287cc73333e5374a61bbebc69bf
Konrad Reiche <konrad.reiche@gmail.com>
2013-10-02 23:45:20 +0200

    Add partial to display metadata record content
    I am not sure whether this is a good idea, due to different places where
    the content has to be looked up, but I have added a partial to display
    relevant record content.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

--- app/models/validation.rb
+++
@@ -1,9 +0,0 @@
-module Validation
-
-  def self.uniform_sample(repository, metric, n, idx=0)
-    total = repository.total
-    metadata = repository.sort_metric_scores(metric, :asc, total)
-    metadata.each_slice(total / n).to_a.map { |partition| partition[idx].to_hash }
-  end
-
-end
