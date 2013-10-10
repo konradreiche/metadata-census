2ab7b03ca8cb56282ed4c4e2c7c4ebe201d268cb
Konrad Reiche <konrad.reiche@gmail.com>
2013-10-10 05:06:46 +0200

    Add histogram to display quality distribution
    A histogram has now been added to the snapshot view. I have commented
    out the hiding of the x-axis line. I had hidden it for the bar charts
    on the metric view, but I think in the case of the histogram it adds
    a lot of value to the design. I might add the line to the metric view's
    bar charts again, too.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

--- app/models/analyzer/quality_distribution.rb
+++ app/models/analyzer/quality_distribution.rb
@@ -4,12 +4,7 @@ module Analyzer
 
     def analyze(snapshot)
       metadata = snapshot.metadata_records.without(:record)
-      scores = metadata.map(&:score).map { |score| score * 100 }
-      grouped = scores.group_by { |score| (score / 10).to_i }
-
-      grouped.each_with_object({}) do |(group, values), distribution|
-        distribution[group * 10] = values.length
-      end
+      metadata.map(&:score).map { |score| score * 100 }
     end
 
   end
