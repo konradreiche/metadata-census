b1c18344806e1dbd7565e94b8d31ef21c0ae4a1a
Konrad Reiche <konrad.reiche@gmail.com>
2013-10-27 21:51:42 +0100

    Start to design repository import unit test
    I have added a separate repository test file in order to establish a
    spec file for the repository import.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

--- spec/controllers/admin/repositories_controller_spec.rb
+++ spec/controllers/admin/repositories_controller_spec.rb
@@ -2,10 +2,22 @@ require 'spec_helper'
 
 describe Admin::RepositoriesController do
 
-  describe "GET 'index'" do
-    it "returns http success" do
-      get 'index'
-      response.should be_success
+  describe "GET index" do
+    it "renders the index template" do
+      get "index"
+      expect(response).to render_template("index")
+    end
+  end
+
+  describe "GET new" do
+    it "renders the new template" do
+      get "new"
+      expect(response).to render_template("new")
+    end
+  end
+
+  describe "POST create" do
+    it "stores the repositories in the database" do
     end
   end
 
--- spec/controllers/repositories_controller_spec.rb
+++ spec/controllers/repositories_controller_spec.rb
@@ -2,10 +2,11 @@ require 'spec_helper'
 
 describe RepositoriesController do
 
-  describe "GET 'index'" do
+  describe "GET index" do
     it "returns http success" do
       get 'index'
       response.should be_success
+      expect(response).to render_template('index')
     end
   end
 
