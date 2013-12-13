c8fcaf79fc7229a5178ec0ae1ff85ec06d39b5af
Konrad Reiche <konrad.reiche@gmail.com>
2013-12-13 08:25:49 +0100

    Fix dropdown menu creation for the repository show
    For the repository show page I still needed to adjust the rendering of
    the dropdown menu. This worked nicely and I have added a test as well to
    cover this case.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

--- spec/views/shared/_dropdown_menu_spec.rb
+++ spec/views/shared/_dropdown_menu_spec.rb
@@ -2,13 +2,26 @@ require 'spec_helper'
 
 describe "shared dropdown menu partial" do
 
+  it "displays the repository menu for repositories" do
+    repositories = FactoryGirl.create_list(:repositories, 3)
+    repository = repositories.first
+
+    options = { entities: [repositories], display: repository }
+    rendered = render partial: 'shared/dropdown_menu', locals: options
+    expect(rendered).to have_selector('a[data-toggle]', repository.to_s)
+    repositories.each do |entity|
+      path = repository_path(entity)
+      expect(rendered).to have_link(entity.to_s, href: path)
+    end
+  end
+
   it "displays the repository menu for repositories and snapshots" do
     repositories = FactoryGirl.create_list(:repositories, 3, :with_snapshots)
     repository = repositories.first
     snapshot = repository.snapshots.first
 
-    locals = { entities: [repositories, snapshot], display: repository }
-    rendered = render partial: 'shared/dropdown_menu', locals: locals
+    options = { entities: [repositories, snapshot], display: repository }
+    rendered = render partial: 'shared/dropdown_menu', locals: options
 
     expect(rendered).to have_selector('a[data-toggle]', repository.to_s)
     repositories.each do |entity|
@@ -24,8 +37,8 @@ describe "shared dropdown menu partial" do
     snapshots = repository.snapshots.to_a
     snapshot = snapshots.first
 
-    locals = { entities: [repository, snapshots], display: snapshot }
-    rendered = render partial: 'shared/dropdown_menu', locals: locals
+    options = { entities: [repository, snapshots], display: snapshot }
+    rendered = render partial: 'shared/dropdown_menu', locals: options
 
     expect(rendered).to have_selector('a[data-toggle]', snapshot.to_s)
     snapshots.each do |entity|
