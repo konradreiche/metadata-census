f40db0856c45b1f147aecbfd6873ea103cc2fa74
Konrad Reiche <konrad.reiche@gmail.com>
2013-12-24 01:42:28 +0100

    Prepare paginated view for the accuracy metric
    Since the results of the accuracy metric's analysis I want to add
    pagination for the view. The content is delivered at once, but only a
    portion is visible at once until the user selects another page. I
    started to slice the analysis result hash and create multiple tables.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

--- app/models/analyzer/accuracy.rb
+++ app/models/analyzer/accuracy.rb
@@ -2,13 +2,13 @@ class Analyzer::Accuracy
 
   def self.analyze(snapshot, metric)
     metadata = MetadataRecord.where(snapshot: snapshot)
-    metadata = metadata.only("record.id", "#{metric}.analysis")
+    metadata = metadata.only("record.id", "#{metric.id}.analysis")
 
     metadata.each_with_object({}) do |document, analysis|
-      if document[metric].nil?
+      if document[metric.id].nil?
         analysis[document.record['id']] = []
       else
-        analysis[document.record['id']] = document[metric]['analysis']
+        analysis[document.record['id']] = document[metric.id]['analysis']
       end
     end
   end
